f2eef4be548058eba947fb22ed166c1d73cadb71
syed samad ali <samadali0125@gmail.com>
2024-08-17 13:08:28 +0530

    Solved some Intermediate Patterns

---
+++ pattern10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pattern10 {
+     public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter  a numbe of rows to print the pattern");
+        int n = scn.nextInt();
+
+        for(int i=1; i<=n; i++) {
+
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+
+
+        }
+
+
+
+
+     }
+}
