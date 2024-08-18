232bb1ae1e5a9c44514187cf5e60226ed7953969
syed samad ali <samadali0125@gmail.com>
2024-08-18 10:53:14 +0530

    solve functions questions

--- Prime_number.java
+++ Prime_number.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Prime_number {
       public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
+
         System.out.println("Enter a number");
         int number = scn.nextInt();
         int count = 0;
